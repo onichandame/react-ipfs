35811229eb8f689e417c5c594272dc1fd03c6c67
onichandame <zxinmyth@gmail.com>
2020-08-21 09:09:44 +0000

    Deploying to gh-pages from  @ f5d7cd626a26acef098fa370fb63071f027bc1b0 🚀

---
+++ precache-manifest.2ef4d1b43dea3d9b6ed1a3c496f5dab5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1017c9f426585ed66733bf914decb82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b055a888669749619f1c",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4978f65c4362a3b58b46",
+    "url": "./static/js/2.e95f8116.chunk.js"
+  },
+  {
+    "revision": "c2295f8584f6d942bba14cb30a21b9cf",
+    "url": "./static/js/2.e95f8116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b055a888669749619f1c",
+    "url": "./static/js/main.99978c14.chunk.js"
+  },
+  {
+    "revision": "b12fb06c1b19e2084157",
+    "url": "./static/js/runtime-main.e89f0be9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
