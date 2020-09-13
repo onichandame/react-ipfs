aa971a4f4d6e3b8b96a81b4d68b956a7b642df64
onichandame <zxinmyth@gmail.com>
2020-09-13 06:18:06 +0000

    removed dummy

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { CssBaseline } from '@material-ui/core'
 import { SnackbarProvider } from 'notistack'
 
-import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
