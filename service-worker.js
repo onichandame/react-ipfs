01245b7c8f0efedd61dd9c6dafa369db52ac73a3
onichandame <zxinmyth@gmail.com>
2020-09-16 06:59:09 +0000

    Deploying to gh-pages from  @ 6e957fb0fbe69dea8c08454565119d9c9e0588ab 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0edf3f1353f98e83a761bc52bd71c2e6.js"
+  "./precache-manifest.db05f090e50619aa1c9d226728cd3e11.js"
 );
 
 self.addEventListener('message', (event) => {
