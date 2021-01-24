b6001e4e7f8e6a16e3e402f213a321efd2d5f301
onichandame <zxinmyth@gmail.com>
2021-01-24 15:14:54 +0000

    done storybook

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import React, {
+  FC,
+  useContext,
+  createContext,
+  useState,
+  useEffect,
+} from 'react';
+import createHttpClient from 'ipfs-http-client';
+
+type Ipfs = Promise<ReturnType<typeof createHttpClient> | null>;
+
+type ExternalArgs = Parameters<typeof createHttpClient>[0];
+
+const useIpfsInstance = (args: ExternalArgs): Ipfs => {
+  const [ipfs, setIpfs] = useState<Ipfs>(Promise.resolve(null));
+
+  useEffect(() => {
+    const ipfs = createHttpClient(args);
+    setIpfs(Promise.resolve(ipfs));
+
+    return () => setIpfs(Promise.resolve(null));
+  }, [args]);
+
+  return ipfs;
+};
+
+const Context = createContext<Ipfs>(Promise.resolve(null));
+
+export const IpfsProvider: FC<{
+  opts: Parameters<typeof useIpfsInstance>[0];
+}> = ({ children, opts }) => {
+  const ipfs = useIpfsInstance(opts);
+  return <Context.Provider value={ipfs}>{children}</Context.Provider>;
+};
+
+export const useIpfs = () => {
+  const ipfs = useContext(Context);
+  return ipfs;
+};
---
+++ stories/index.stories.tsx
@@ -0,0 +1,294 @@
+import { makeStyles } from '@material-ui/core/styles'
+import React, {
+  useCallback,
+  MouseEvent,
+  useContext,
+  createContext,
+  FC,
+  useEffect,
+  useState,
+  ContextType,
+} from 'react'
+import {
+  Divider,
+  Grid,
+  Menu,
+  MenuItem,
+  Badge,
+  IconButton,
+  Typography,
+  AppBar,
+  Toolbar,
+} from '@material-ui/core'
+import {
+  Group,
+  SignalCellularConnectedNoInternet0Bar,
+  SignalCellular0Bar,
+  SignalCellular1Bar,
+  SignalCellular2Bar,
+  SignalCellular3Bar,
+  SignalCellular4Bar,
+} from '@material-ui/icons'
+import { Meta, Story } from '@storybook/react'
+
+import { useIpfs, IpfsProvider } from '../src'
+
+const randStr = () =>
+  Math.random()
+    .toString(36)
+    .substr(2, 5)
+
+const Status = createContext(`UNKNOWN` as 'UNKNOWN' | 'RUNNING' | 'ERROR')
+const Id = createContext(``)
+const Peers = createContext(
+  [] as {
+    addr: any
+    peer: string
+    // only if verbose: true
+    latency?: string
+    muxer: string
+    // only if verbose: true
+    streams?: string[]
+    direction: number
+  }[]
+)
+const PeerNum = createContext(0)
+
+const Wrapper: FC = ({ children }) => {
+  const ipfsPromise = useIpfs()
+  const [id, setId] = useState<ContextType<typeof Id>>(``)
+  const [status, setStatus] = useState<ContextType<typeof Status>>(`UNKNOWN`)
+  const [peers, setPeers] = useState<ContextType<typeof Peers>>([])
+  const [peerNum, setPeerNum] = useState<ContextType<typeof PeerNum>>(0)
+  const checkStatus = useCallback(
+    (err?: Error) => {
+      ipfsPromise
+        .then(ipfs => ipfs.id())
+        .then(() => status !== `RUNNING` && setStatus(`RUNNING`))
+        .catch((e: Error) => {
+          status !== `ERROR` && setStatus(`ERROR`)
+          throw err || e
+        })
+    },
+    [ipfsPromise, status]
+  )
+  useEffect(() => {
+    checkStatus()
+  }, [checkStatus])
+  // update basic info regularly
+  useEffect(() => {
+    const reset = () => {
+      setId(``)
+      setPeers([])
+    }
+    let jobs: ReturnType<typeof setInterval>[] = []
+    switch (status) {
+      case `RUNNING`:
+        ipfsPromise
+          .then(ipfs => {
+            if (ipfs && ipfs.id)
+              ipfs.id().then(({ id }: { id: string }) => setId(id))
+            if (ipfs && ipfs.swarm && ipfs.swarm.peers) {
+              jobs.push(
+                setInterval(() => {
+                  ipfs.swarm.peers().then((prs: any[]) => setPeers(prs))
+                }, 1000)
+              )
+            }
+          })
+          .catch(e => checkStatus(e))
+        break
+      default:
+        reset()
+    }
+    return () => jobs.forEach(job => clearInterval(job))
+  }, [status, ipfsPromise, checkStatus])
+  // update derived info regularly
+  useEffect(() => {
+    const newPeerNum = peers.length
+    if (newPeerNum !== peerNum) setPeerNum(newPeerNum)
+  }, [peers, peerNum])
+  useEffect(() => {
+    console.log(status)
+  }, [status])
+  return (
+    <Id.Provider value={id}>
+      <Status.Provider value={status}>
+        <Peers.Provider value={peers}>
+          <PeerNum.Provider value={peerNum}>{children}</PeerNum.Provider>
+        </Peers.Provider>
+      </Status.Provider>
+    </Id.Provider>
+  )
+}
+
+const NavBar: FC = () => {
+  const ipfsPromise = useIpfs()
+  const [menuAnchor, setMenuAnchor] = useState<HTMLElement | null>(null)
+  const [peersAnchor, setPeersAnchor] = useState<HTMLElement | null>(null)
+  const [menuId] = useState(randStr())
+  const [peersId] = useState(randStr())
+  const [showNum] = useState(10)
+  const peerNum = useContext(PeerNum)
+  const peers = useContext(Peers)
+
+  const openMenu = (e: MouseEvent<HTMLElement>) => {
+    setMenuAnchor(e.currentTarget)
+  }
+  const closeMenu = () => {
+    setMenuAnchor(null)
+  }
+
+  const openPeers = (e: MouseEvent<HTMLElement>) => {
+    setPeersAnchor(e.currentTarget)
+  }
+  const closePeers = () => {
+    setPeersAnchor(null)
+  }
+
+  return (
+    <div style={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            IPFS
+          </Typography>
+          <div style={{ flexGrow: 1 }} />
+          <IconButton
+            color="inherit"
+            aria-label="ipfs peers"
+            aria-controls={peersId}
+            aria-haspopup="true"
+            onClick={openPeers}
+          >
+            <Badge badgeContent={peerNum}>
+              <Group />
+            </Badge>
+          </IconButton>
+          <IconButton
+            color="inherit"
+            aria-label="ipfs node"
+            aria-controls={menuId}
+            aria-haspopup="true"
+            onClick={openMenu}
+          >
+            {peerNum <= 0 ? (
+              <SignalCellularConnectedNoInternet0Bar />
+            ) : peerNum < 10 ? (
+              <SignalCellular0Bar />
+            ) : peerNum < 64 ? (
+              <SignalCellular1Bar />
+            ) : peerNum < 128 ? (
+              <SignalCellular2Bar />
+            ) : peerNum < 256 ? (
+              <SignalCellular3Bar />
+            ) : (
+              <SignalCellular4Bar />
+            )}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Menu
+        anchorEl={menuAnchor}
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={menuId}
+        keepMounted
+        open={!!menuAnchor}
+        onClose={closeMenu}
+      >
+        <MenuItem
+          onClick={() => {
+            closeMenu()
+            ipfsPromise.then(ipfs => ipfs && ipfs.stop())
+          }}
+        >
+          Stop
+        </MenuItem>
+      </Menu>
+      <Menu
+        anchorEl={peersAnchor}
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={peersId}
+        keepMounted
+        open={!!peersAnchor}
+        onClose={closePeers}
+      >
+        <MenuItem onClick={closePeers}>
+          <Grid container direction="row" spacing={2} justify="space-around">
+            <Grid item>total number: {peerNum}</Grid>
+          </Grid>
+        </MenuItem>
+        <Divider />
+        {peers.slice(0, showNum).map(peer => (
+          <MenuItem
+            key={randStr()}
+            onClick={() => {
+              closePeers()
+            }}
+          >
+            {peer.addr.toString()}
+          </MenuItem>
+        ))}
+        <Divider />
+        {peerNum > showNum && (
+          <MenuItem>
+            <Grid container direction="row" justify="space-around">
+              <Grid item>more</Grid>
+            </Grid>
+          </MenuItem>
+        )}
+      </Menu>
+    </div>
+  )
+}
+
+type Props = {
+  host: string
+  protocol: 'http' | 'https'
+  port: number
+}
+
+const Root: FC<Props> = ({ host, protocol, port }) => {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.paper,
+    },
+  }))
+  const styles = useStyles()
+  return (
+    <IpfsProvider opts={{ protocol, host, port }}>
+      <Wrapper>
+        <div className={styles.root}>
+          <NavBar />
+        </div>
+      </Wrapper>
+    </IpfsProvider>
+  )
+}
+
+export default {
+  title: 'IPFS',
+  component: Root,
+  argTypes: {
+    host: {
+      control: { type: `text` },
+    },
+    port: { control: { type: `number`, min: 1 } },
+    protocol: { control: { type: `text` } },
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+} as Meta<Props>
+
+const Template: Story<Props> = args => <Root {...args} />
+
+// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
+// https://storybook.js.org/docs/react/workflows/unit-testing
+export const Example = Template.bind({})
+Example.args = { host: `localhost`, port: 5001, protocol: `http` }
